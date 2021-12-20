83f8f2c25316f9cf7010a1d569f668e8dfa1b6c6
it.chhaya <it.chhaya@gmail.com>
2021-12-20 13:18:22 +0700

    update map

--- app/src/main/java/com/devkh/mapdemo/MainActivity.java
+++ app/src/main/java/com/devkh/mapdemo/MainActivity.java
@@ -2,10 +2,19 @@ package com.devkh.mapdemo;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,33 +25,97 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
 
     private Button mBtnDirection;
+
+    private Location mLocation;
+    private LocationManager mLocationManager;
+    private boolean mIsGPSEnabled;
+    private boolean mIsNetworkEnabled;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        try {
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
         mBtnDirection = findViewById(R.id.btn_direction);
 
+        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        mIsGPSEnabled = mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        mIsNetworkEnabled = mLocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        if (mIsGPSEnabled) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[] {
+                        android.Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION
+                }, 101);
+            }
+            mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                    500, 0F, this);
+            mLocation = mLocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            double lat = mLocation.getLatitude();
+            double lon =mLocation.getLongitude();
+            Log.i("TAG", "onCreate: " + lat);
+            Log.i("TAG", "onCreate: " + lon);
+        }
+
         // bind event
-        getKshrdDirection();
+        getDirection();
 
     }
 
-    private void getKshrdDirection() {
+    private void getDirection() {
         mBtnDirection.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Uri gmmIntentUri = Uri.parse("https://www.google.com/maps/dir/?api=1&destination=11.575985764067013,104.88940561703062");
+                // Get direction with Google Map App
+                /*Uri gmmIntentUri = Uri.parse("https://www.google.com/maps/dir/?api=1&destination=11.575985764067013,104.88940561703062");
                 Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
                 mapIntent.setPackage("com.google.android.apps.maps");
-                startActivity(mapIntent);
+                startActivity(mapIntent);*/
+
+                Intent intent = new Intent(MainActivity.this, PitchesActivity.class);
+                //intent.putExtra("lat", m);
+                startActivity(intent);
             }
         });
     }
 
 
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        Log.i("TAG", "onLocationChanged: " + location.getLatitude());
+        Log.i("TAG", "onLocationChanged: " + location.getLongitude());
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull List<Location> locations) {
+
+    }
+
+    @Override
+    public void onFlushComplete(int requestCode) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+
+    }
 }
--- app/src/main/java/com/devkh/mapdemo/PitchesActivity.java
+++ app/src/main/java/com/devkh/mapdemo/PitchesActivity.java
@@ -1,8 +1,18 @@
 package com.devkh.mapdemo;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,11 +21,18 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.devkh.mapdemo.databinding.ActivityPitchesBinding;
+import com.google.android.gms.tasks.Task;
 
-public class PitchesActivity extends FragmentActivity implements OnMapReadyCallback {
+public class PitchesActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
+
+    private final static String TAG = PitchesActivity.class.getName();
 
     private GoogleMap mMap;
     private ActivityPitchesBinding binding;
+
+    private LocationManager mLocationManager;
+    private double mLat;
+    private double mLong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +41,24 @@ public class PitchesActivity extends FragmentActivity implements OnMapReadyCallb
         binding = ActivityPitchesBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {"android.Manifest.permission.ACCESS_FINE_LOCATION", "android.Manifest.permission.ACCESS_COARSE_LOCATION"}, 1);
+        }
+
+        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        /*if (!mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            //OnGPS();
+            Toast.makeText(this, "Try to on the GPS", Toast.LENGTH_SHORT).show();
+        } else {
+            getLocation();
+        }*/
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
     /**
@@ -42,16 +73,50 @@ public class PitchesActivity extends FragmentActivity implements OnMapReadyCallb
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        getLocation();
         // Add a marker in Sydney and move the camera
-        LatLng kshrd = new LatLng(11.576879184150627, 104.88936269978221);
+        LatLng kshrd = new LatLng(mLat, mLong);
+        LatLng rooy7 = new LatLng(11.583795800432265, 104.88750895924561);
+        LatLng tSoccer = new LatLng(11.586142747234472, 104.90252228770636);
+        LatLng premium = new LatLng(11.589782162416592, 104.8971432920974);
+
 
         // Set type of map to display
         // mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
         mMap.addMarker(new MarkerOptions().position(kshrd).title("Korea Software HRD Center"));
+        mMap.addMarker(new MarkerOptions().position(rooy7).title("Rooy7 Sport Club"));
+        mMap.addMarker(new MarkerOptions().position(tSoccer).title("T-Soccer TK"));
+        mMap.addMarker(new MarkerOptions().position(premium).title("Premium Sport Club"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(kshrd));
         mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
         mMap.setTrafficEnabled(true);
     }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        mLat = location.getLatitude();
+        mLong = location.getLongitude();
+        Log.i(TAG, "Lat, Long = (" + mLat + ", " + mLong + ")");
+    }
+
+    private void getLocation() {
+        Log.i(TAG, "getLocation: ");
+        if (ActivityCompat.checkSelfPermission(
+                this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "Request Permission");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 100);
+        } else {
+            Log.i(TAG, "Get location");
+            mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                    500,
+                    0F, this);
+            Location location = mLocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            Log.i(TAG, "getLocation: " + location);
+            mLat = location.getLatitude();
+            mLong = location.getLongitude();
+        }
+    }
+
 }
